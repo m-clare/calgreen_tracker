8ff4a58e841273e69e16ffe7d6bed79c7254d5cf
Maryanne Wachter <maryanne.wachter@gmail.com>
2024-06-11 21:22:58 -0700

    Initial data loader - date filter - LA data api

---
+++ src/index.md
@@ -0,0 +1,11 @@
+---
+toc: false
+---
+
+```js
+const permits = FileAttachment("la_permits_issued.parquet").parquet();
+```
+
+```js 
+Inputs.table(permits)
+```
---
+++ src/la_permits_issued.parquet.sh
@@ -0,0 +1,16 @@
+URL="https://data.lacity.org/resource/pi9x-tg5x.csv?\$where=issue_date>'2024-06-01T00:00:00.000'"
+
+# Use the data loader cache directory to store the downloaded data.
+TMPDIR="src/.observablehq/cache/"
+
+# Download the data
+curl "$URL" -o "$TMPDIR/$CODE.csv"
+
+# Generate a Parquet file using DuckDB.
+duckdb :memory: << EOF
+COPY (
+  SELECT *
+  FROM read_csv('$TMPDIR/$CODE.csv')
+  WHERE true
+) TO STDOUT (FORMAT 'parquet', COMPRESSION 'gzip');
+EOF
